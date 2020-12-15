08faf3588a81562824afb67d2fff5a8ef4e1d0f3
jfspps <jfsapps@gmail.com>
2020-12-15 17:10:10 +0000

    impl ViewChild template-to-TS extraction

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,15 +9,23 @@ export class CockpitComponent implements OnInit {
   newServerName = '';
   newServerContent = '';
 
+  // use ViewChild decorator; newServerContent is now of type ElementRef (a reference to an element, not the element itself)
+  @ViewChild('serverInputContent', {static: true}) newServerContentChild: ElementRef;
+
   // required to enable other components to execute their methods when a cockpit method/event has occurred
   // the EventEmitter effectively casts the property as an event
   // the @Output decorator passess data from child to parent (cockpit to app; compare to @Input which passes from parent to child)
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   
-  constructor() { }
+  constructor() {
+   }
 
   ngOnInit(): void {
+    // use a ViewChild to access template data from TS, in this case, without methods or events
+    // general advice is not to change template values with ViewChild, just access them
+    console.log("ViewChild: " + this.newServerContentChild);
+    console.log("ViewChild value: " + this.newServerContentChild.nativeElement.value);
   }
 
   onAddServer(newServerName) {
